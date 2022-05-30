8b9f87d4b66bbc8bf757a74377e8a4ecffdd7095
Nicoxuatgia <106530604+Nicoxuatgia@users.noreply.github.com>
2022-05-30 19:56:34 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* Fig. 2.1:fig02_01.c
+A first program i C*/
+#include <stdio.h>
+
+/*function main begins program execution */
+int main( void)
+{
+printf("welcom to C!\n" );
+
+return 0; /* indicate that program ended successfully */
+} /* end function main */
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
